68164659969ab596a7fe9c79abb9ea8ef6fc962a
Rao <Prashanth.Rao@philips.com>
2021-05-07 16:51:22 +0530

    Added base CronJobService

---
+++ VaccinationInfoProvider/VaccinationInfoFetcherService/CronJobService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Cronos;
+using Microsoft.Extensions.Hosting;
+
+namespace VaccinationInfoProvider.VaccinationInfoFetcherService {
+    public class CronJobService: IHostedService, IDisposable {
+
+        private System.Timers.Timer timer;
+        private readonly CronExpression _expression;
+        private readonly TimeZoneInfo _timeZoneInfo;
+
+
+        public CronJobService() {
+            _expression = CronExpression.Parse("*/5****");
+            _timeZoneInfo = TimeZoneInfo.Local;
+        }
+
+        /// <inheritdoc />
+        public async Task StartAsync(CancellationToken cancellationToken) {
+            await ScheduleJob(cancellationToken);
+        }
+
+        /// <inheritdoc />
+        public async Task StopAsync(CancellationToken cancellationToken) {
+            timer?.Stop();
+            await Task.CompletedTask;
+        }
+
+
+        protected virtual async Task ScheduleJob(CancellationToken cancellationToken) {
+            var next = _expression.GetNextOccurrence(DateTimeOffset.Now, _timeZoneInfo);
+
+            if (next.HasValue) {
+                var delay = next.Value - DateTimeOffset.Now;
+                // prevent non-positive values from being passed into Timer
+                if (delay.TotalMilliseconds <= 0) {
+                    await ScheduleJob(cancellationToken);
+                }
+
+                timer = new System.Timers.Timer(delay.TotalMilliseconds);
+                timer.Elapsed += async (sender, args) => {
+                    timer.Dispose();  // reset and dispose timer
+                    timer = null;
+
+                    if (!cancellationToken.IsCancellationRequested) {
+                        await DoWork(cancellationToken);
+                    }
+
+                    if (!cancellationToken.IsCancellationRequested) {
+                        await ScheduleJob(cancellationToken);    // reschedule next
+                    }
+                };
+                timer.Start();
+            }
+            await Task.CompletedTask;
+        }
+
+        public virtual async Task DoWork(CancellationToken cancellationToken) {
+            await Task.Delay(5000, cancellationToken);  // do the work
+        }
+
+
+        protected virtual void Dispose(bool disposing) {
+            timer.Dispose();
+        }
+
+        /// <inheritdoc />
+        public void Dispose() {
+            Dispose(true);
+        }
+
+        /// <summary>
+        /// Finalizer
+        /// </summary>
+        ~CronJobService() {
+            Dispose(false);
+        }
+    }
+}
---
+++ VaccinationInfoProvider/VaccinationInfoFetcherService/VaccinationInfoFetcherService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+
+namespace VaccinationInfoProvider.VaccinationInfoFetcherService {
+
+    /// <summary>
+    /// Responsibility => Background service for talking to CoWin API's.
+    /// </summary>
+    public class VaccinationInfoFetcherService
+    {
+        
+    }
+}
